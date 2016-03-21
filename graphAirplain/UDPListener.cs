b358bc07ef69246b3fccaa53a6ba0ed91e164a5f
Иван Ермаков <vancha94@gmail.com>
2016-03-22 00:24:42 +0300

    Добавлена возможность чтения данных по UDP протоколу в один поток

--- graphAirplain/UDPListener.cs
+++ graphAirplain/UDPListener.cs
@@ -2,12 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
 
 namespace graphAirplain
 {
     class UDPListener
     {
 
+        private const int listenPort = 256;
+        public UDPListener()
+        {
+
+        }
+
+
+        public void StartListener()
+        {
+            UdpClient listener = new UdpClient(listenPort);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+
+            try
+            {
+                while (!done)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+                    byte[] bytes = listener.Receive(ref groupEP);
+
+                    Console.WriteLine("Received broadcast from {0} :\n {1}\n",
+                        groupEP.ToString(),
+                        Encoding.ASCII.GetString(bytes, 0, bytes.Length));
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                listener.Close();
+            }
+
+        }
     }
 }
